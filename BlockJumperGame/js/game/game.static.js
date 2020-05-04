66e797c9f679c8fd43fcc18578b093a6f1a79024
HelgaBK <olga.karpyshyn@gmail.com>
2020-05-05 00:11:18 +0300

    added Course Project

---
+++ BlockJumperGame/js/game/game.static.js
@@ -0,0 +1,16 @@
+window.game = window.game || {};
+
+window.game.static = {
+	colors: {
+		white: 0xffffff,
+		darkGreen: 0x0c0f14,
+		darkerMidGreen:  0x808000,
+		midGreen: 0x85a064,
+		midBrightGreen: 0x6eb069,
+		brightYellow: 0xFFFF00,
+		superBrightGreen: 0xdaffae,
+		midBlue: 0x008080,
+		midGreenBlue: 0x0c0f14,
+		yellow: 0xff7800
+	}
+};
